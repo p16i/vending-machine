dd8680b6fcee15420f02926261e94d8b37803aaa
Pattarawat Chormai <pat.chormai@gmail.com>
2012-08-18 18:48:06 +0700

    Add running method.

--- classes/VendingMachine.py
+++ classes/VendingMachine.py
@@ -23,7 +23,82 @@ class VendingMachine :
             res['status'] = 0
         return res
 
-    # Encapsulate Attr
+    # End Sell
+
+    def running( self ):
+        stock = self.__stock
+
+        flag = 1
+
+        print '#####################################'
+        print '##   Welcome to Vending Machine    ##'
+        print '#####################################'
+
+        while flag :
+            print '## Would you like to drink today ? ##'
+            print '#####################################'
+
+            for i in range( 0, len(stock) ):
+                text = '# ['+str(i+1)+'] '+stock.keys()[i].name()
+                text += '\t'+'$'+str(stock.keys()[i].price())
+
+                if( stock.values()[i] == 0 ):
+                    text += '\t*Out of Stock'
+
+                print text
+
+            command = raw_input('# Please choose your goods no.\n or type `x` to exit | ') #Get Input
+
+            # Check input
+            if( is_int_try(command) and int(command) in range( 1, len(stock)+1 ) ):
+                # Sell Goods
+                i = int(command)
+                product = stock.keys()[i-1]
+                flag = 1
+                total_money = 0
+
+                print '## Your order '+product.name() + ' ##'
+                print '## Please insert money $' + str( product.price() )+ ' or'
+                print '## Press `b` to buy or'
+                print '## Press `x` to exit'
+                while flag:
+
+                    c = raw_input('Total : '+ str(total_money)+ ' | ')
+
+                    if( is_int_try(c) ):
+                        total_money += int(c)
+                    elif( c == 'b' ):
+                        res = self.sell( product, total_money)
+                        if( res.get('status') ):
+                            print '#####################################'
+                            print '#####################################'
+                            print '##       Have a Nice day           ##'
+                            print 'Return | '+ str(res.get('change'))
+                            print '#####################################'
+                            print '#####################################'
+                            break
+                        else:
+                            print '!! Something wrong'
+
+                    elif( c == 'x' ):
+                        print '#####################################'
+                        print '##       Return money              ##'
+                        print '#####################################'
+                        break
+
+
+            elif( command == 'x' ):
+                # Exit
+                print '##      Machine Closed      ##'
+                flag = 0
+            else:
+                print '#####################################'
+                print '##       Invalid Command           ##'
+                print '#####################################'
+
+    #End Running
+
+## Encapsulate Attr
 
     # Stock
     def stock( self ):
@@ -32,3 +107,11 @@ class VendingMachine :
     def add_stock( self, product, amount ):
         self.__stock[product] = amount
 
+
+# Helper method
+def is_int_try( str ):
+    try:
+        int(str)
+        return True
+    except ValueError:
+        return False
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print '# Welcome to Vending Machine'
+from classes.VendingMachine import *
+from classes.Goods import *
+
+## Intial data ##
+v = VendingMachine()
+
+pepsi = Goods( 'PEPSI' ,10 )
+oishi = Goods( 'OISHI' ,20 )
+
+v.add_stock( pepsi, 10 )
+v.add_stock( oishi, 0 )
+
+v.running() # Run the machine
+
+
