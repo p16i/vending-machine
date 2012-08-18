b050b96dd7844bbd9dce1bea0354886aa5fc691a
Pattarawat Chormai <pat.chormai@gmail.com>
2012-08-18 15:44:11 +0700

    Extend class vending machine

--- classes/VendingMachine.py
+++ classes/VendingMachine.py
@@ -2,5 +2,26 @@
 # By Pattarawat Chormai
 
 class VendingMachine :
-    def sell():
-        return true
+
+    # Init method
+    def __init__(self):
+
+        # Ex: stock = { $PRODUCT_OBJ : $LEFT }
+        self.__stock = dict()
+
+    # Sell method
+    def sell( self, order, money ):
+        res = dict()
+        res['status'] = 1
+        res['change'] = money - order[1]
+        return res
+
+    # Encapsulate Attr
+
+    # Stock
+    def stock():
+        return self.__stock;
+
+    def add_stock( self, product, amount ):
+        self.__stock[product] = 10
+
--- t/test_vending_machine.py
+++ t/test_vending_machine.py
@@ -2,12 +2,25 @@
 
 import unittest
 import os.path
-from classes.VendingMachine import VendingMachine
+from classes.VendingMachine import *
 
 class TestVendingMachine( unittest.TestCase ):
-    def test_can_use_vending_machine( self ):
-        my_machine = VendingMachine();
-        self.assertTrue( my_machine.sell, "We can sell" );
+
+    def setUp( self ):
+        self.my_machine = VendingMachine()
+
+        self.assertIsNotNone( self.my_machine );
+
+    def test_buy( self ):
+        # Add stock
+        product = ( 'PEPSI', 20 )
+        self.my_machine.add_stock( product, 10 );
+
+        # By PEPSI
+        result = self.my_machine.sell( product, 100 )
+        self.assertTrue( result['status'], "We can sell" );
+        self.assertEqual( result['change'], 80, "Got collect change" );
+
 
 if __name__ == '__main__':
     unittest.main()
